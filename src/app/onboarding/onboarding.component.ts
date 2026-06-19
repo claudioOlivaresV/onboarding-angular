18e08b9dca630accd972e434308dd67b0583fc93
Claudio Olivares <ca.olivares91@gmail.com>
2026-06-18 20:26:19 -0400

    feat: se agregan clases y angular material

--- src/app/onboarding/onboarding.component.ts
+++ src/app/onboarding/onboarding.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatSlideToggle } from '@angular/material/slide-toggle';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MatCheckboxModule],
   templateUrl: './onboarding.component.html',
   styleUrl: './onboarding.component.scss',
 })
