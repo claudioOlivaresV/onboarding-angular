2937ed93c2b874552e2237f24a43724038256b7e
Claudio Olivares <ca.olivares91@gmail.com>
2026-06-23 21:08:22 -0400

    feat: mejoras ui

--- src/app/comprobante/comprobante.component.ts
+++ src/app/comprobante/comprobante.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+type Status = 'loading' | 'success' | 'error';
 
 @Component({
   selector: 'app-comprobante',
-  imports: [],
+  imports: [MatIconModule, MatButtonModule, MatCardModule],
   templateUrl: './comprobante.component.html',
-  styleUrl: './comprobante.component.scss'
+  styleUrl: './comprobante.component.scss',
 })
 export class ComprobanteComponent {
+  status = signal<Status>('loading');
 
+  ngOnInit() {
+    setTimeout(() => {
+      this.status.set('success');
+    }, 2000); // 200 segundos
+  }
+
+  reset() {
+    this.status.set('loading');
+  }
 }
--- src/app/resumen/resumen.component.ts
+++ src/app/resumen/resumen.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-resumen',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './resumen.component.html',
-  styleUrl: './resumen.component.scss'
+  styleUrl: './resumen.component.scss',
 })
 export class ResumenComponent {
+  constructor(private router: Router) {}
 
+  activar(): void {
+    this.router.navigate(['/comprobante']);
+  }
 }
