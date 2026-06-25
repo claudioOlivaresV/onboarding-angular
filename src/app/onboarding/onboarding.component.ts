c0df746e6fcda415172b1c4a728ca5cd531624cd
Claudio Olivares <ca.olivares91@gmail.com>
2026-06-24 20:58:08 -0400

    feat: mejoras diseño

--- src/app/onboarding/onboarding.component.ts
+++ src/app/onboarding/onboarding.component.ts
@@ -3,11 +3,19 @@ import { RouterOutlet } from '@angular/router';
 import { MatSlideToggle } from '@angular/material/slide-toggle';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { CardComponent } from '../components/card/card.component';
+import { LucideAngularModule, CreditCard } from 'lucide-angular';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatCheckboxModule, CardComponent],
+  imports: [
+    RouterOutlet,
+    MatCheckboxModule,
+    CardComponent,
+    LucideAngularModule,
+  ],
   templateUrl: './onboarding.component.html',
   styleUrl: './onboarding.component.scss',
 })
-export class OnboardingComponent {}
+export class OnboardingComponent {
+  CreditCard = CreditCard;
+}
